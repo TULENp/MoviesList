26aef819b143cb7a024cc5ecbcf358d365a5b51b
TULENp <zh_kononenko3@mail.ru>
2023-07-05 21:23:47 +0300

    Add pagination and pull refresh

--- src/screens/MoviesListScreen.tsx
+++ src/screens/MoviesListScreen.tsx
@@ -36,9 +36,8 @@ export function MoviesListScreen() {
     ]
     const [sortType, setSortType] = React.useState<string>(sortTypes[0].key);
 
+    //add sort select to header
     useEffect(() => {
-        getAllMovies(sortType);
-        //add "Settings" button to header
         setOptions({
             headerRight: () => (
                 <SelectList
@@ -57,12 +56,23 @@ export function MoviesListScreen() {
                 />
             )
         });
+    }, [])
+
+    // get movies when currentPage changes 
+    useEffect(() => {
+        getAllMovies(sortType, currentPage);
+    }, [currentPage])
+
+    //get movies on 1st page when sort type changes
+    useEffect(() => {
+        setCurrentPage(1);
+        getAllMovies(sortType, 1);
     }, [sortType])
 
-    async function getAllMovies(sortType: string) {
+    async function getAllMovies(sortType: string, currentPage: number) {
         setIsLoading(true);
 
-        const moviesData = await GetAllMovies(sortType);
+        const moviesData = await GetAllMovies(sortType, currentPage);
         if (moviesData.results) {
             setMoviesList(moviesData.results)
             setCurrentPage(moviesData.page)
@@ -73,11 +83,21 @@ export function MoviesListScreen() {
         setIsLoading(false);
     }
 
+    function toPrevPage() {
+        if (currentPage > 1) {
+            setCurrentPage(prev => prev - 1)
+        }
+    }
+
+    function toNextPage() {
+        if (currentPage < 1000) {
+            setCurrentPage(prev => prev + 1)
+        }
+    }
 
     return (
         <View style={styles.container}>
-            {isLoading
-                ?
+            {isLoading ?
                 <>
                     <ActivityIndicator style={{ marginTop: 200 }} size="large" />
                     <Text style={{ fontSize: 20, marginTop: 10 }}>Загрузка...</Text>
@@ -98,10 +118,23 @@ export function MoviesListScreen() {
                         <FlatList
                             data={moviesList}
                             numColumns={2}
+                            refreshing={isLoading}
+                            onRefresh={() => getAllMovies(sortType, currentPage)}
                             renderItem={({ item }) =>
                                 <Pressable onPress={() => navigate('Movie', { movie: item })}>
                                     <MovieCard movie={item} />
                                 </Pressable>
+                            }
+                            ListFooterComponent={
+                                <View style={styles.toolbar}>
+                                    <Pressable style={styles.button} onPress={toPrevPage}>
+                                        <Text>Назад</Text>
+                                    </Pressable>
+                                    <Text>{currentPage}</Text>
+                                    <Pressable style={styles.button} onPress={toNextPage}>
+                                        <Text>Вперед</Text>
+                                    </Pressable>
+                                </View>
                             }
                         />
                     )
@@ -116,6 +149,23 @@ const styles = StyleSheet.create({
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    toolbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    button: {
+        borderColor: 'black',
+        borderWidth: 1,
+        width: '30%',
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
     },
 })
 
